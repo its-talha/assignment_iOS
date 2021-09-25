722f4cb8c6ba127bba70182427597c2f431bcb1a
Mohammad Talha <mohammad@Satyams-MacBook-Pro.local>
2021-09-26 00:41:05 +0530

    addded second assignment

---
+++ assignment_21-sept/AssignmentApp/AssignmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentApp
+//
+//  Created by Mohammad Talha on 25/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
