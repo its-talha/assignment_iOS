7b57916c1adccf457c5d8213362450e5d005e0d1
Mohammad Talha <mohammad@Satyams-MacBook-Pro.local>
2021-10-20 17:38:43 +0530

    syncing files

---
+++ assignment_08-oct/iOSAssessment-12/iOSAssessment-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAssessment-12
+//
+//  Created by Mohammad Talha on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
