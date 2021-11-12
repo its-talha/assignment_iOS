22a0b82505d1083772325f4c070619b9f3a2af8e
Mohammad Talha <mohammad@Satyams-MacBook-Pro.local>
2021-11-12 23:35:05 +0530

    added assginments

---
+++ assignment_1-nov/DataPersistenceDemo/DataPersistenceDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataPersistenceDemo
+//
+//  Created by Mohammad Talha on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
